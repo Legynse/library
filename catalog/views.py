8c2459784cdae11fa8c9fa9b43e2fedd2b4af663
legynse <jeniiamin@yandex.ru>
2020-10-21 19:06:18 +0300

    Baida

---
+++ catalog/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
+
+from django.views import generic
+
+
+def index(request):
+        num_books = Book.objects.all().count()
+        num_instances = BookInstance.objects.all().count()
+        num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+        num_authors = Author.objects.count()
+        return render(
+            request,
+            'index.html',
+            context={'num_books': num_books, 'num_instances': num_instances,
+                     'num_instances_available': num_instances_available,
+                     'num_authors': num_authors},
+        )
+
+
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'my_book_list'
+    # queryset = Book.objects.filter(title__icontains='war')[:5]
+    template_name = 'books/my_arbitrary_template_name_list.html'
+
+    paginate_by = 1
+
+
+    # def get_queryset(self):
+    #    return Book.objects.filter(title__icontains='war')[:5]
+
+    def get_context_data(self, **kwargs):
+        context = super(BookListView, self).get_context_data(**kwargs)
+        context['some_data'] = 'This is just some data'
+        return context
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
